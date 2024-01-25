1af3354ef1a484e2ae43a779c8f0ca51f70c12f2
Rena Hajjar <81925703+rena-hajjar@users.noreply.github.com>
2024-01-25 11:46:29 -0500

    split main function into two, remove tool column since only using ultrasound

--- VGG_To_DeepLearnLive.py
+++ VGG_To_DeepLearnLive.py
@@ -3,32 +3,46 @@ import argparse
 
 
 def main(args):
-    # look into parsers so i don't have to manually change this every time
     vggRegions = pd.read_csv(args.source_csv_file)
     lblFile = pd.read_csv(args.target_csv_file)
 
-    # initializing new columns
+    # initializing new column
     lblFile["Tool_and_US"] = ["" for i in lblFile.index]
-    lblFile['Tool bounding box'] = [[] for j in lblFile.index]
 
     for i in vggRegions.index:
-        # are the vgg and label file indices the same?
+        # get the name of the current frame
         filename = vggRegions['filename'][i]
 
+        # get the class (tool type) and shape (bounding box attributes)
         regionShape = eval(vggRegions['region_shape_attributes'][i])
         className = eval(vggRegions['region_attributes'][i])
+
+        # as long as there is a bounding box on the frame, split attributes into dict
         if bool(className):
             tool = "ultrasound"
             bbox = {"class": className['class'], 'xmin': regionShape["x"], "ymin": regionShape["y"],
                     'xmax': regionShape["x"] + regionShape["width"], 'ymax': regionShape["y"] + regionShape["height"]}
+            
+            match_row_to_bbox(lblFile, filename, bbox)
 
-            indexedRow = lblFile.loc[lblFile['FileName'] == filename]
-            index = indexedRow.iloc[0, 0]
-
-            lblFile.loc[index, 'Tool bounding box'].append(bbox)
-            lblFile.loc[index, 'Tool_and_US'] = tool
-
+            
     lblFile.to_csv(args.target_csv_file, index=False)
+
+
+def match_row_to_bbox(lblFile, filename, bbox):
+    """
+    This function matches the bounding box just created to the corresponding row in the label csv file, without
+    assuming they have the same indices
+    :param lblFile: the final file that is being changed to include the new bounding box column
+    :param filename: name to match the row to
+    :param bbox: the bounding box attributes to be written to the label file
+    """
+    indexedRow = lblFile.loc[lblFile['FileName'] == filename]
+    index = indexedRow.iloc[0, 0]
+
+    lblFile.loc[index, 'Tool bounding box'].append(bbox)   
+
+    return lblFile
 
 
 def get_arguments():
